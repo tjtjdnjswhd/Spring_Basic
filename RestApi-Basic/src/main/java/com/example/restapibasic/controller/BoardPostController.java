9fa8c5b507a70a9c3db5c77bea996c53d39b4f00
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-07-18 15:28:16 +0900

    RUD

--- RestApi-Basic/src/main/java/com/example/restapibasic/controller/BoardPostController.java
+++ RestApi-Basic/src/main/java/com/example/restapibasic/controller/BoardPostController.java
@@ -1,13 +1,13 @@
 package com.example.restapibasic.controller;
 
 import com.example.restapibasic.domain.BoardPostDto;
+import com.example.restapibasic.domain.CommentDto;
 import com.example.restapibasic.service.BoardPostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/board-post")
@@ -23,5 +23,43 @@ public class BoardPostController {
     public ResponseEntity<BoardPostDto> createBoardPost(@RequestBody BoardPostDto boardPostDto) {
         BoardPostDto createdBoardPost = boardPostService.createBoardPost(boardPostDto);
         return ResponseEntity.ok(createdBoardPost);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<BoardPostDto>> getAllBoardPosts() {
+        List<BoardPostDto> boardPostDtos = boardPostService.getAllBoardPosts();
+        return ResponseEntity.ok(boardPostDtos);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BoardPostDto> getBoardPostById(@PathVariable(name = "id") Long id) {
+        BoardPostDto foundBoardPost = boardPostService.getBoardPostById(id);
+        return ResponseEntity.ok(foundBoardPost);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteBoardPost(@PathVariable(name = "id") Long id) {
+        boardPostService.deleteBoardPost(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<BoardPostDto> updateBoardPost(@PathVariable("id") Long id, @RequestBody
+    BoardPostDto updateBoardPostDto) {
+        BoardPostDto updatedBoardPostDto = boardPostService.updateBoardPost(id, updateBoardPostDto);
+
+        return ResponseEntity.ok(updatedBoardPostDto);
+    }
+
+    @PostMapping("/{postId}/comments")
+    public ResponseEntity<CommentDto> createComment(@PathVariable("postId") Long postId, @RequestBody CommentDto createCommentDto) {
+        CommentDto createdCommentDto = boardPostService.createComment(postId, createCommentDto);
+        return ResponseEntity.ok(createCommentDto);
+    }
+
+    @DeleteMapping("/{postId}/comments/{commentId}")
+    public ResponseEntity<Void> deleteComment(@PathVariable("postId") Long postId, @PathVariable("commentId") Long commentId) {
+        boardPostService.deleteComment(postId, commentId);
+        return ResponseEntity.noContent().build();
     }
 }
--- RestApi-Basic/src/main/java/com/example/restapibasic/service/BoardPostService.java
+++ RestApi-Basic/src/main/java/com/example/restapibasic/service/BoardPostService.java
@@ -1,12 +1,12 @@
 package com.example.restapibasic.service;
 
-import com.example.restapibasic.controller.BoardPostController;
 import com.example.restapibasic.domain.BoardPost;
 import com.example.restapibasic.domain.BoardPostDto;
 import com.example.restapibasic.domain.Comment;
 import com.example.restapibasic.domain.CommentDto;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,21 +17,84 @@ public class BoardPostService {
     private Long nextPostId = 1L;
     private Long nextCommentId = 1L;
 
+    public List<BoardPostDto> getAllBoardPosts() {
+        return boardPosts.stream()
+                .map(BoardPostService::convertToBoardPostDto)
+                .collect(Collectors.toList());
+    }
+
+    public BoardPostDto getBoardPostById(Long id) {
+        return boardPosts.stream()
+                .filter(boardPost -> boardPost.getId().equals(id))
+                .map(BoardPostService::convertToBoardPostDto)
+                .findFirst()
+                .orElse(null);
+    }
+
     public BoardPostDto createBoardPost(BoardPostDto boardPostDto) {
-        BoardPost boardPost = new BoardPost();
+        BoardPost boardPost = convertToBoardPostEntity(boardPostDto);
         boardPost.setId(nextPostId++);
+        boardPost.setCreatedAt(LocalDateTime.now());
+        boardPost.setUpdatedAt(LocalDateTime.now());
+        boardPosts.add(boardPost);
+        return convertToBoardPostDto(boardPost);
+    }
+
+    public void deleteBoardPost(Long id) {
+        BoardPost foundBoardPost = findBoardPostById(id);
+        boardPosts.remove(foundBoardPost);
+    }
+
+    public BoardPostDto updateBoardPost(Long id, BoardPostDto updateBoardPostDto) {
+        BoardPost boardPost = findBoardPostById(id);
+        boardPost.setTitle(updateBoardPostDto.getTitle());
+        boardPost.setContent(updateBoardPostDto.getContent());
+        boardPost.setUpdatedAt(LocalDateTime.now());
+        return convertToBoardPostDto(boardPost);
+    }
+
+    public CommentDto createComment(Long postId, CommentDto createCommentDto) {
+        BoardPost boardPost = findBoardPostById(postId);
+        Comment comment = convertToCommentEntity(createCommentDto);
+        comment.setId(nextCommentId++);
+        comment.setCreatedAt(LocalDateTime.now());
+        boardPost.addComment(comment);
+        return convertToCommentDto(comment);
+    }
+
+    public void deleteComment(Long postId, Long commentId) {
+        BoardPost boardPost = findBoardPostById(postId);
+        Comment comment = findCommentById(commentId, boardPost);
+        boardPost.removeComment(comment);
+    }
+
+    private static Comment findCommentById(Long commentId, BoardPost boardPost) {
+        return boardPost.getComments().stream()
+                .filter(c -> c.getId().equals(commentId))
+                .findFirst()
+                .orElseThrow(()->new IllegalArgumentException("댓글을 찾을 수 없습니다."));
+    }
+
+    private BoardPost findBoardPostById(Long id) {
+        return boardPosts.stream()
+                .filter(boardPost -> boardPost.getId().equals(id))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new);
+    }
+
+    private static BoardPost convertToBoardPostEntity(BoardPostDto boardPostDto) {
+        BoardPost boardPost = new BoardPost();
         boardPost.setTitle(boardPostDto.getTitle());
         boardPost.setContent(boardPostDto.getContent());
         boardPost.setAuthor(boardPostDto.getAuthor());
-
         if (boardPostDto.getComments() != null) {
             boardPostDto.getComments().forEach(commentDto -> {
-                Comment comment = convertToComment(commentDto);
+                Comment comment = convertToCommentEntity(commentDto);
+                comment.setBoardPost(boardPost);
                 boardPost.addComment(comment);
             });
         }
-
-        return boardPostDto;
+        return boardPost;
     }
 
     private static BoardPostDto convertToBoardPostDto(BoardPost boardPost) {
@@ -42,10 +105,29 @@ public class BoardPostService {
         boardPostDto.setAuthor(boardPost.getAuthor());
         boardPostDto.setCreatedAt(boardPost.getCreatedAt());
         boardPostDto.setUpdatedAt(boardPost.getUpdatedAt());
+
         if (boardPost.getComments() != null) {
             boardPostDto.setComments(
-                    boardPost.getComments().stream().map(this::convertToCommentDto).collect(Collectors.toList()));
+                    boardPost.getComments().stream().map(BoardPostService::convertToCommentDto)
+                            .collect(Collectors.toList())
+            );
         }
         return boardPostDto;
+    }
+
+    private static Comment convertToCommentEntity(CommentDto commentDto) {
+        Comment comment = new Comment();
+        comment.setId(commentDto.getId());
+        comment.setContent(commentDto.getContent());
+        comment.setAuthor(commentDto.getAuthor());
+        return comment;
+    }
+
+    private static CommentDto convertToCommentDto(Comment comment) {
+        CommentDto commentDto = new CommentDto();
+        commentDto.setId(comment.getId());
+        commentDto.setContent(comment.getContent());
+        commentDto.setAuthor(comment.getAuthor());
+        return commentDto;
     }
 }
