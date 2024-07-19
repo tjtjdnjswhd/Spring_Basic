259d9450b7a96c16b13c496da30cf292af73bc14
tjtjdnjswhd <tjtjdnjswhd@gmail.com>
2024-07-19 15:37:44 +0900

    JPA basic

---
+++ Jpa-MySql-Basic/src/main/java/com/example/jpamysqlbasic/BookRepository.java
@@ -0,0 +1,18 @@
+package com.example.jpamysqlbasic;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByIsbn(String isbn);
+
+    List<Book> findByPriceBetween(Double minPrice, Double maxPrice);
+
+    List<Book> findByStockGreaterThanEqual(Integer minStock);
+
+    List<Book> findByTitleContaining(String keyword);
+}
---
+++ Jpa-MySql-Basic/src/main/java/com/example/jpamysqlbasic/JpaMySqlBasicApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamysqlbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaMySqlBasicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaMySqlBasicApplication.class, args);
+    }
+
+}
